954e0682a731357dbc76ac9e009522cb1b8762f9
ashbornofthehell <breycy15@gmail.com>
2024-11-22 15:16:23 -0500

    Añadido el script setup.sh para automatización

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone <URL del repositorio >
+git clone https://github.com/Ashbornofthehell/jenkins-pipeline-setup 
 cd jenkins-pipeline-setup
 docker-compose up -d
 echo "Jenkins está corriendo en http://localhost:8080"
